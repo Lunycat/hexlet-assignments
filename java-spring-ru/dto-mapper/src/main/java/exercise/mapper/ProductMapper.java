0486fbcac1974e601f0137a281c9dc6cd2151d33
@hexlet/cli <support@hexlet.io>
2024-12-22 10:33:31 +0300

    submit java-spring-ru/dto-mapper

---
+++ java-spring-ru/dto-mapper/src/main/java/exercise/mapper/ProductMapper.java
@@ -0,0 +1,27 @@
+package exercise.mapper;
+
+import exercise.dto.ProductCreateDTO;
+import exercise.dto.ProductDTO;
+import exercise.dto.ProductUpdateDTO;
+import exercise.model.Product;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.mapstruct.MappingConstants;
+import org.mapstruct.MappingTarget;
+
+
+@Mapper(componentModel = MappingConstants.ComponentModel.SPRING)
+public interface ProductMapper {
+    @Mapping(target = "title", source = "name")
+    @Mapping(target = "price", source = "cost")
+    @Mapping(target = "vendorCode", source = "barcode")
+    ProductDTO map(Product model);
+
+    @Mapping(target = "name", source = "title")
+    @Mapping(target = "cost", source = "price")
+    @Mapping(target = "barcode", source = "vendorCode")
+    Product map(ProductCreateDTO dto);
+
+    @Mapping(target = "cost", source = "price")
+    void update(ProductUpdateDTO dto, @MappingTarget Product model);
+}
