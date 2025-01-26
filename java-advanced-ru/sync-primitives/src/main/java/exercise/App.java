b43319b99db1125fc9099582e69c59c209df095c
@hexlet/cli <support@hexlet.io>
2025-01-26 18:43:49 +0300

    submit java-advanced-ru/sync-primitives

---
+++ java-advanced-ru/sync-primitives/src/main/java/exercise/App.java
@@ -0,0 +1,18 @@
+package exercise;
+
+class App {
+
+    public static void main(String[] args) throws InterruptedException {
+        SafetyList safetyList = new SafetyList();
+
+        ListThread listThread1 = new ListThread(safetyList);
+        ListThread listThread2 = new ListThread(safetyList);
+
+        listThread1.start();
+        listThread2.start();
+
+        listThread1.join();
+        listThread2.join();
+    }
+}
+
